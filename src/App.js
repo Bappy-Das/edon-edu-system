2974a4d2418bd1ef4f882a6d0ff0d1df756ed684
musasazibe <musasazib@gmail.com>
2022-02-16 16:00:08 +0600

    Add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar'
+
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Navbar/>
       <h1>Hello Edon and Educational site</h1>
     </div>
   );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar>
+                <Container>
+                    <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">Mark Otto</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
