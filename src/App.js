2bc609b6dffdb1396b91561080ec2016f559118c
Bappy-Das <bappydas.bd51@gmail.com>
2022-02-21 02:11:20 +0600

    firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Header from './components/Shared/Header/Header';
+import Navbar from './components/Shared/Navbar/Navbar';
+import Footer from './components/Shared/Footer/Footer';
 
 
 
@@ -11,12 +14,15 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="home" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
