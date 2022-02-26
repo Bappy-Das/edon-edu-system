3b2ec83d1ab08a7c9b9ef05c46e099043fb8b24f
Mohammad Shakib <mdshakib105100@gmail.com>
2022-02-26 22:39:03 +0600

    Setup Blog Folder

---
+++ src/components/AllBlog/Blogs/Blogs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Blog from '../Blog/Blog';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h1>Blogss</h1>
+            <Blog></Blog>
+        </div>
+    );
+};
+
+export default Blogs;
