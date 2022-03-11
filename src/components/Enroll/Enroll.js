c66a5d5d71cc267a36174e2a32fb901f1ebe909f
musasazibe <musasazib@gmail.com>
2022-03-11 10:52:01 +0600

    Add course details

--- src/components/Enroll/Enroll.js
+++ src/components/Enroll/Enroll.js
@@ -18,38 +18,40 @@ const Enroll = () => {
     }, [courseId, details])
 
     return (
-        <div className='container'>
-            <h1 className="pt-5 pb-5 header-style">
-                {details?.courseName}
-            </h1>
-            <div className="row">
-                <div className="col-12 col-md-8">
-                    <h3 className='course-details-title'>Description</h3>
-                    <p className='course-details-paragraph course-details'>{details?.description}</p>
-                    <br />
-                    <h3 className='course-details-title'>Requirements</h3>
-                    <p className='course-details-paragraph course-details'>{details?.requirments}</p>
-                    <br />
-                    <h3 className='course-details-title'>What you'll learn</h3>
-                    <p className='course-details-paragraph course-details'>{details?.whylearn}</p>
-                </div>
-                <div className="col-12 col-md-4 enroll-bg">
-                    <img className='enroll-img' src={details?.image} alt="" />
-                    <div className='container'>
-                        <h2 className='enroll-price'>$ {details?.price}</h2>
-                        <p className='course-includes'>This course includes:</p>
-                        <ul className='course-li'>
-                        <li>1.5 hours on-demand video</li>
-                        <li>1 article</li>
-                        <li>6 downloadable resources</li>
-                        <li>1 practice test</li>
-                        <li>Full lifetime access</li>
-                        <li>Access on mobile and TV</li>
-                        <li>Assignments</li>
-                        <li>Certificate of completion</li>
-                        <li>NASBA CPE credits: 2.4</li>
-                    </ul>
-                        <button className=" mt-4 btn-enroll">Add to Cart</button>
+        <div className='course-details-bg'>
+            <div className='container'>
+                <h1 className="pt-5 pb-5 header-style">
+                    {details?.courseName}
+                </h1>
+                <div className="row">
+                    <div className="col-12 col-md-8">
+                        <h3 className='course-details-title'>Description</h3>
+                        <p className='course-details-paragraph course-details'>{details?.description}</p>
+                        <br />
+                        <h3 className='course-details-title'>Requirements</h3>
+                        <p className='course-details-paragraph course-details'>{details?.requirments}</p>
+                        <br />
+                        <h3 className='course-details-title'>What you'll learn</h3>
+                        <p className='course-details-paragraph course-details'>{details?.whylearn}</p>
+                    </div>
+                    <div className="col-12 col-md-4 enroll-bg">
+                        <img className='enroll-img' src={details?.image} alt="" />
+                        <div className='container'>
+                            <h2 className='enroll-price'>$ {details?.price}</h2>
+                            <p className='course-includes'>This course includes:</p>
+                            <ul className='course-li'>
+                                <li>1.5 hours on-demand video</li>
+                                <li>1 article</li>
+                                <li>6 downloadable resources</li>
+                                <li>1 practice test</li>
+                                <li>Full lifetime access</li>
+                                <li>Access on mobile and TV</li>
+                                <li>Assignments</li>
+                                <li>Certificate of completion</li>
+                                <li>NASBA CPE credits: 2.4</li>
+                            </ul>
+                            <button className=" mt-4 mb-5 btn-enroll">Add to Cart</button>
+                        </div>
                     </div>
                 </div>
             </div>
